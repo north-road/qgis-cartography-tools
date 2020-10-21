fd2319b638ba25bc8db483cbfefdcd2dfa0cd9ae
Nyall Dawson <nyall.dawson@gmail.com>
2020-10-21 10:49:05 +1000

    Bump version

--- cartography_tools/processing/provider.py
+++ cartography_tools/processing/provider.py
@@ -61,7 +61,7 @@ class CartographyToolsProvider(QgsProcessingProvider):
         """
         Provider plugin version
         """
-        return "1.0.0"
+        return "1.0.1"
 
     def id(self):
         """
