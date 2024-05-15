a53f3f0aac14320fd7e5df975eaea1445a59b836
Till Frankenbach <Tillfrankenbach@web.de>
2024-05-15 04:57:29 -0400

    Replace QFontMetrics.width with QFontMetrics.horizontalAdvance

--- cartography_tools/tools/marker_settings_widget.py
+++ cartography_tools/tools/marker_settings_widget.py
@@ -37,7 +37,7 @@ class MarkerSettingsWidget(BASE, WIDGET):
 
         self.code_combo.setEditable(True)
         self.code_combo.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
-        self.code_combo.setMinimumWidth(QFontMetrics(self.font()).width('X') * 40)
+        self.code_combo.setMinimumWidth(QFontMetrics(self.font()).horizontalAdvance('X') * 40)
 
         self.field_code_combo.setAllowEmptyFieldName(True)
 
